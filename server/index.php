b0e21a7d1b63239432de21ad1727fff0d1afb2fd
Komiya Yu <komiya.yu@n138.jp>
2025-01-21 06:23:54 +0900

    Access-Control-Expose-Headers

--- server/index.php
+++ server/index.php
@@ -4,6 +4,8 @@ header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding, Origin, Accept, Access-Control-Allow-Headers, Authorization, X-Requested-With, X-Token');
+header('Access-Control-Expose-Headers: *');
+
 header('X-Powered-By: Hidden');
 
 const COMPOSER_FILE = './vendor/autoload.php';
