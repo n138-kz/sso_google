7a40928ddad52ee4f38af3feb5c4a052f5cef847
Komiya Yu <komiya.yu@n138.jp>
2025-01-21 06:24:49 +0900

    header: Server: Hidden

--- server/index.php
+++ server/index.php
@@ -6,6 +6,7 @@ header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding, Origin, Accept, Access-Control-Allow-Headers, Authorization, X-Requested-With, X-Token');
 header('Access-Control-Expose-Headers: *');
 
+header('Server: Hidden');
 header('X-Powered-By: Hidden');
 
 const COMPOSER_FILE = './vendor/autoload.php';
